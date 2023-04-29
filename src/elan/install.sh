672e2255680624cea8d5e127ce71ea82d72cc8e4
Logan <10239411+Loag@users.noreply.github.com>
2023-04-28 21:57:09 -0400

    initial commit

---
+++ src/elan/install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+
+# Maybe install curl
+which curl > /dev/null || (apt update && apt install curl -y -qq)
+
+curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/leanprover/elan/master/elan-init.sh | sh
