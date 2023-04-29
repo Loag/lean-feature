244e8c4b41e7167f0253a0d8c6166f5f87976844
Logan <10239411+Loag@users.noreply.github.com>
2023-04-28 22:02:39 -0400

    run non interactive

--- src/elan/install.sh
+++ src/elan/install.sh
@@ -4,4 +4,6 @@ set -e
 # Maybe install curl
 which curl > /dev/null || (apt update && apt install curl -y -qq)
 
-curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/leanprover/elan/master/elan-init.sh | sh
+curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/leanprover/elan/master/elan-init.sh
+
+elan-init.sh -y
